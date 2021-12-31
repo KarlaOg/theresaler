d7e26ebd3e713ed70a863771ea9332c4c1983b8e
Emeline GARO <emeline.garo@supinternet.fr>
2021-12-31 15:44:28 +0100

    added fixture user

---
+++ api/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        //ADMIN
+        $admin = new User;
+        $hash = $this->encoder->hashPassword($admin, "password");
+        $admin->setEmail("admin@admin.com")
+            ->setFirstname("admin")
+            ->setLastname("admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+
+        //USERS
+        for ($u = 0; $u < 20; $u++) {
+            $user = new User();
+            $hash = $this->encoder->hashPassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName())
+                ->setPassword($hash)
+                ->setRoles(['ROLE_USER']);
+            $users[] = $user;
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}

3fc45d2ba1506bf69fcf45dcd6ce6eaac396f216
Emeline Garo <emeline.garo@supinternet.fr>
2022-01-03 20:13:49 +0100

    add register post (#8)
    * add register post
    
    * wip

--- front-vue/src/views/Register.vue
+++ front-vue/src/views/Register.vue
@@ -20,7 +20,7 @@
 </template>
 <script>
 export default {
-  data() {
+  data: function () {
     return {
       firstname: '',
       lastname: '',
@@ -30,59 +30,19 @@ export default {
   },
   methods: {
     register() {
-      this.$store.dispatch('register', {
-        firstname: this.firstname,
-        lastname: this.lastname,
-        email: this.email,
-        password: this.password,
-      });
+      this.$store
+        .dispatch('register', {
+          firstname: this.firstname,
+          lastname: this.lastname,
+          email: this.email,
+          password: this.password,
+        })
+        .catch((err) => {
+          this.errors = console.log(err.response.data.errors);
+        });
     },
   },
 };
 </script>
 
-<style>
-/* Add padding to containers */
-.container {
-  padding: 16px;
-}
-
-/* Full-width input fields */
-input[type='text'],
-input[type='email'],
-input[type='password'] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type='text']:focus,
-input[type='password']:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit/register button */
-.btn {
-  background-color: #04aa6d;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.btn:hover {
-  opacity: 1;
-}
-
-/* Add a blue text color to links */
-a {
-  color: dodgerblue;
-}
-</style>
+<style></style>

ea13997bad9e5af51349f89253b3f5f4b949d6c5
Karla Og <karla@karla.home>
2022-02-14 22:21:01 +0100

    [DONE] ADDED HELMET TO API

--- bid/app.js
+++ bid/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express()
 const port = 3001
 const bet = require('./routes/bet.route')
+let helmet = require('helmet')
 
 app.use(express.json())
+app.use(helmet())
 // app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1', bet)
 
